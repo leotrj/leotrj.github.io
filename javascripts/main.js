1ede011a7ed0a67328c0f88cd8054d7fd2242bc7
Leo <lt342@cornell.edu>
2015-01-30 10:21:44 -0500

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,39 +1,19 @@
-var main = function(){
-    $('.dropdown-toggle').click(function(){
-        $('.dropdown-menu').toggle();
-    });
-    
-    $('.arrow-next').click(function(){
-        var currentSlide = $('.active-slide');
-        var nextSlide = currentSlide.next();        
-        var currentDot = $('.active-dot');
-        var nextDot = currentDot.next();
-                
-        if(nextSlide.length == 0){
-            nextSlide = $('.slide').first();
-            nextDot = $('.dot').first();
-        }
-        
-        currentSlide.fadeOut(600).removeClass('active-slide');
-        nextSlide.fadeIn(600).addClass('active-slide');
-        currentDot.removeClass('active-dot');
-        nextDot.addClass('active-dot');
-    });
-    
-    $('.arrow-prev').click(function(){
-        var currentSlide = $('.active-slide');
-        var prevSlide = currentSlide.prev();
-        var currentDot = $('.active-dot');
-        var prevDot = currentDot.prev();
-        
-        if(prevSlide.length == 0){
-            prevSlide = $('.slide').last();
-            prevDot = $('.dot').last();
-        }
-        currentSlide.fadeOut(600).removeClass('active-slide')
-        prevSlide.fadeIn(600).addClass('active-slide');
-        currentDot.removeClass('active-dot');
-        prevDot.addClass('active-dot');
-    });
+$(function() {
+	smoothScroll(600);
+	
+});
+
+// smoothScroll function is applied from the document ready function
+function smoothScroll (duration) {
+	$('a[href^="#"]').on('click', function(event) {
+
+	    var target = $( $(this).attr('href') );
+
+	    if( target.length ) {
+	        event.preventDefault();
+	        $('html, body').animate({
+	            scrollTop: target.offset().top
+	        }, duration);
+	    }
+	});
 }
-$(document).ready(main);
